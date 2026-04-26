4617e2d3c082cb70367588f0f5478626a322c30f
patpat <tomaspatrickdanielle@gmail.com>
2026-04-26 21:27:30 +0800

    feat: create data for groups

---
+++ my-app/src/lib/data/groups.ts
@@ -0,0 +1,12 @@
+import { Group } from '@/lib/types'
+
+export const groups: Group[] = [
+    { name: 'BTS', color: '#b39ddb', type: 'bg'},
+    { name: 'BLACKPINK', color: '#f48fb1', type: 'gg'},
+    { name: 'TWICE', color: '#ffab91', type: 'gg'},
+    { name: 'AESPA', color: '#80cbc4', type: 'gg'},
+    { name: 'NEWJEANS', color: '#81d4fa', type: 'gg'},
+    { name: 'STAYC', color: '#ce93d8', type: 'gg'},
+    { name: 'IVE', color: '#ffcc80', type: 'gg'},
+    { name: 'STRAY KIDS', color: '#90caf9', type: 'bg'},
+]
