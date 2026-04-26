d5806d42ec64ea2a56ada28d04826c185dec10ae
patpat <tomaspatrickdanielle@gmail.com>
2026-04-26 21:27:40 +0800

    feat: initial data for idols

---
+++ my-app/src/lib/data/idols.ts
@@ -0,0 +1,37 @@
+import { Idol } from '@/lib/types'
+
+export const idols: Idol[] = [
+    {
+        id: 'rm',
+        name: 'RM',
+        group: 'BTS',
+        groupType: 'bg',
+        birthday: '1994-09-12',
+        nationality: 'South Korea',
+        flag: '🇰🇷',
+        profileColor: '#b39ddb',
+        imageUrl: 'https://kprofiles.com/wp-content/uploads/2016/04/RM.webp'
+    },
+    {
+        id: 'jennie',
+        name: 'Jennie',
+        group: 'BLACKPINK',
+        groupType: 'gg',
+        birthday: '1996-01-16',
+        nationality: 'South Korea',
+        flag: '🇰🇷',
+        profileColor: '#f48fb1',
+        imageUrl: 'https://kprofiles.com/wp-content/uploads/2016/08/Jennie-Blackpink-2025.jpg'
+    },
+    {
+        id: 'nayeon',
+        name: 'Nayeon',
+        group: 'TWICE',
+        groupType: 'gg',
+        birthday: '1995-09-22',
+        nationality: 'South Korea',
+        flag: '🇰🇷',
+        profileColor: '#ffab91',
+        imageUrl: 'https://kprofiles.com/wp-content/uploads/2016/05/NAYEON-1-900x643.jpeg'
+    }
+]
