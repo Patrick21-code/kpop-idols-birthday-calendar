575bb5a8d51e54093f2f6c1f0bc3212987b96358
patpat <tomaspatrickdanielle@gmail.com>
2026-04-27 21:28:57 +0800

    test: test useAppStore file

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,8 +1,18 @@
+'use client'
+
+import { useAppStore } from '@/lib/store/useAppStore'
 
 export default function Home() {
-  return (
-    <div>
+  const { activeGroups, filterType, currentMonth } = useAppStore()
 
-    </div>
+  return (
+    <main>
+      <p>Active groups: {activeGroups.join(', ')}</p>
+      <p>Filter: {filterType}</p>
+      <p>Month: {currentMonth.toLocaleString('default', { month: 'long', year: 'numeric' })}</p>
+    </main>
   );
 }
+
+
+//toLocaleString converts a Date object into a string
